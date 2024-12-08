25410cda0038b751e51ce98b5b9649e4facc84fe
Endervan Alves <endvan@gmail.com>
2024-12-08 13:10:51 -0300

    footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,15 +6,17 @@ import {PokeHeaderComponent} from './poke-header/poke-header.component';
 import {PokeSearchComponent} from './poke-search/poke-search.component';
 import {PokeListComponent} from './poke-list/poke-list.component';
 import {RouterModule} from "@angular/router";
+import {PokeFooterComponent} from './poke-footer/poke-footer.component';
 
 
 @NgModule({
   declarations: [
     PokeHeaderComponent,
     PokeSearchComponent,
-    PokeListComponent
+    PokeListComponent,
+    PokeFooterComponent
   ],
-  exports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
+  exports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent, PokeFooterComponent],
   imports: [
     CommonModule,
     RouterModule
